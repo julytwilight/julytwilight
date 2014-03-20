fcaabcf953a94761f0945fe28970445378bf2541
Mikasa <mikasayee@neatpath.com>
2014-03-20 21:13:48 +0800

    modify icon

--- mysite/manage.py
+++ mysite/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    debug = True
+    debug = False
 
     if debug:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahead.settings.development")
